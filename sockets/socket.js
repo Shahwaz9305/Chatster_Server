73af34c96271be76afa6c6327abd72865ca2867b
anurag-ay <ay1909069@gmail.com>
2023-09-28 21:13:05 +0530

    Make socket.js more readable and maitainable and easy to debug

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,65 +1,85 @@
+const socket = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
+
+class ChatServer {
+  // Constructor Function
+  constructor(server) {
+    this.io = socket(server, {
+      cors: {
+        origin: ["https://admin.socket.io", "http://localhost:3000"],
+        credentials: true,
+      },
+    });
+    this.onlineUser = new Map();
+    this.setupEvents();
+    instrument(this.io, { auth: false });
+  }
+
+  // Setting up All the events
+  setupEvents() {
+    this.io.on("connection", (socket) => {
+      // On user connect
+      socket.on("addUser", (userId) => this.addUser(userId, socket));
+
+      // Send Message
+      socket.on("sendMessage", (data) => this.sendMessage(data, socket));
+
+      // online Status
+      socket.on("isOnline", (selectedUserId) =>
+        this.isOnline(selectedUserId, socket)
+      );
+
+      // isTyping
+      socket.on("typing", ({ from, to, isTyping }) =>
+        this.isTyping({ from, to, isTyping }, socket)
+      );
+
+      // On disconnect
+      socket.on("disconnect", () => this.disconnect(socket));
+    });
+  }
+
+  addUser(userId, socket) {
+    if (!this.onlineUser.has(userId)) {
+      this.onlineUser.set(userId, socket.id);
+      this.io.emit("userConnectDisconnect", `connect ${socket.id}`);
+    }
+  }
+
+  sendMessage(data, socket) {
+    const userSocketId = this.onlineUser.get(data.to);
+    const { message, timestamp } = data;
+    const newData = { type: "receive", message, timestamp };
+    if (userSocketId) {
+      socket.to(userSocketId).emit("recieveMessage", newData);
+    }
+  }
+
+  isOnline(selectedUserId, socket) {
+    if (this.onlineUser.has(selectedUserId)) {
+      socket.emit("online", "online");
+    } else {
+      socket.emit("online", "");
+    }
+  }
+
+  isTyping({ from, to, isTyping }, socket) {
+    const userId = this.onlineUser.get(to);
+    socket
+      .to(userId)
+      .emit("isTyping", { currentSelectedUser: from, isTyping: isTyping });
+  }
+
+  disconnect(socket) {
+    for (let [key, value] of this.onlineUser.entries()) {
+      if (value === socket.id) {
+        this.onlineUser.delete(key);
+      }
+    }
+    this.io.emit("userConnectDisconnect", `disconnect ${socket.id}`);
+  }
+}
+
 module.exports = function sockets(server) {
-  const socket = require("socket.io");
-  const { instrument } = require("@socket.io/admin-ui");
-
-  const io = socket(server, {
-    cors: {
-      origin: ["https://admin.socket.io", "http://localhost:3000"],
-      credentials: true,
-    },
-  });
-
-  global.onlineUser = new Map();
-  io.on("connection", (socket) => {
-    global.chatSocket = socket;
-
-    // On user connect
-    socket.on("addUser", (userId) => {
-      if (!onlineUser.has(userId)) {
-        onlineUser.set(userId, socket.id);
-        io.emit("userConnectDisconnect", `connect ${socket.id}`);
-      }
-    });
-
-    // Send Message
-    socket.on("sendMessage", (data) => {
-      const userSocketId = onlineUser.get(data.to);
-      const { message, timestamp } = data;
-      const newData = { type: "receive", message, timestamp };
-      if (userSocketId) {
-        socket.to(userSocketId).emit("recieveMessage", newData);
-      }
-    });
-
-    // online Status
-
-    socket.on("isOnline", (selectedUserId) => {
-      if (onlineUser.has(selectedUserId)) {
-        socket.emit("online", "online");
-      } else {
-        socket.emit("online", "");
-      }
-    });
-
-    // isTyping
-
-    socket.on("typing", ({ from, to, isTyping }) => {
-      const userId = onlineUser.get(to);
-      socket
-        .to(userId)
-        .emit("isTyping", { currentSelectedUser: from, isTyping: isTyping });
-    });
-
-    // On disconnect
-    socket.on("disconnect", () => {
-      // remove from online user Map
-      for (let [key, value] of onlineUser.entries()) {
-        if (value === socket.id) {
-          onlineUser.delete(key);
-        }
-      }
-      io.emit("userConnectDisconnect", `disconnect ${socket.id}`);
-    });
-  });
-  instrument(io, { auth: false });
+  new ChatServer(server);
 };
